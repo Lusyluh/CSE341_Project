1bb7a433bfbb32faf59d2e4435431291022ae7d4
Lusyluh <mav19003@byui.edu>
2022-06-25 04:00:17 +0200

    getting recipes from the database

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,17 +1,35 @@
 const mongodb = require('../models/connect');
 
+//get all recipes listed in the database
+const getAll = async (req, res) => {
+    const result = await mongodb.getDb().db('recipeBook').collection('recipes').find();
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists);
+    });
+  };
 //create a new recipe
 //fields/schema: name, ingredients[], method, prep time, cook time, servings, category
 //maybe an image?
 
-const newRecipe = async(req, res) => {
-    const recipe = req.body;
-    try{
+const newRecipe = async (req, res) => {
+    
+    try {
+        const recipe = {
+            name: req.body.name,
+            category: req.body.category,
+            description: req.body.description,
+            prepTime: req.body.prepTime,
+            cookTime: req.body.cookTime,
+            serving: req.body.serving
+        }
         const result = await mongodb.getDb().db('recipeBook').collection('recipes').insertOne(recipe);
-
-    }catch(err){
-        console.log(err.message)
+        console.log(result);
+    } catch (error) {
+        res.status(500).send({message: error.message || 'Recipe could not be created'})
     }
 
 }
-module.exports = {newRecipe};
+module.exports = {
+    getAll, newRecipe
+};
--- routes/recipes.js
+++ routes/recipes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 //connects to the controller
-const recipeController = require()
+const recipeController = require('../controllers/recipes');
 
 //now here are the endpoints
+router.get('/', recipeController.getAll);
 router.post('/', recipeController.newRecipe);
 
 module.exports = router;
