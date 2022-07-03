259acb981021ac61a6a441e6aef0d18951bc2972
Lusyluh <mav19003@byui.edu>
2022-07-03 02:55:06 +0200

    successfully created client id and secret for auth

--- routes/recipes.js
+++ routes/recipes.js
@@ -3,11 +3,21 @@ const router = express.Router();
 
 //connects to the controller
 const recipeController = require('../controllers/recipes');
+const utils = require ('./utils');
 
 //now here are the endpoints
 router.get('/', recipeController.getAll);
 router.post('/',recipeController.newRecipe);
 router.delete('/:id', recipeController.deleteRecipe);
 router.put('/:id', recipeController.updateRecipe)
+//authentication
+app.get ('/auth', async (req, res) => {
+    try {
+      res.redirect (utils.request_get_auth_code_url);
+    } catch (error) {
+      res.sendStatus (500);
+      console.log (error.message);
+    }
+  });
 
 module.exports = router;
